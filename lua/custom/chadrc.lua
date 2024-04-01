182a6c556e6cdaabf9405427e7f8ee8651d48fd2
Ian Price <ianprice943@yahoo.com>
2024-04-01 17:17:34 -0400

    add column 80 highlight, update statusbar to vscode_colorized

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,12 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+  theme = 'catppuccin',
+  statusline = {
+    theme = "vscode_colored"
+  }
+}
 M.plugins = "custom.plugins"
 
 return M
